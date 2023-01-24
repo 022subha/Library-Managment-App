e91551ecf396f3470a94cf67cc894885306e6c12
Subhajit <022subha@gmail.com>
2023-01-24 21:47:13 +0530

    test api created

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 const app = express();
 
+app.get("/test", (req, res) => {
+  res.send("Subhajit");
+});
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
